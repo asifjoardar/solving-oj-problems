db6750a302fe876f114c7d728fc091b4842e3c8a
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-06-29 23:15:54 +0600

    Update C. Number of Ways.cpp

--- codeforce/C/C. Number of Ways.cpp
+++ codeforce/C/C. Number of Ways.cpp
@@ -1,1 +1,27 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+    long long int sum=0,sum1=0,c=0,i,n,p;
+    cin>>n;
+    long long int a[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>p;
+        sum1+=p;
+        a[i]=sum1;
+    }
+    if(!(a[n-1]%3))
+    {
+        p=a[n-1]/3;
+        for(i=0;i<n-1;i++)
+        {
+            if(a[i]==2*p)
+                sum+=c;
+            if(a[i]==p) c++;
+        }
+        cout<<sum<<endl;
+    }
+    else
+        cout<<0<<endl;
+}
