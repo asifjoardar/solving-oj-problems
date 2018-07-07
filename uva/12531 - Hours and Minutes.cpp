aa696da85d97d3d0d52f451ea01c3747f1c397e4
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-07-07 17:30:11 +0600

    Create 12531 - Hours and Minutes.cpp

---
+++ uva/12531 - Hours and Minutes.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+    int n;
+    while(cin>>n)
+    {
+        if(n%6==0)
+            cout<<"Y"<<endl;
+        else
+            cout<<"N"<<endl;
+    }
+}
