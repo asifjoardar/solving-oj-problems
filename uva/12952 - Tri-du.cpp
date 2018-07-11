e04142f2c38efedd1b55e2621653c0c7095998f5
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-07-11 12:43:53 +0600

    Create 12952 - Tri-du.cpp

---
+++ uva/12952 - Tri-du.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+    int a,b;
+    while(scanf("%d%d",&a,&b)!=EOF)
+    {
+        if(a>b) cout<<a<<endl;
+        else if(b>a) cout<<b<<endl;
+        else cout<<a<<endl;
+    }
+}
