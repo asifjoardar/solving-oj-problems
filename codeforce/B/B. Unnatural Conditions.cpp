4dbda986c78ad1816c2145ee322c301ab6a96124
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-08-28 03:04:51 +0600

    Create B. Unnatural Conditions.cpp

---
+++ codeforce/B/B. Unnatural Conditions.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	int n,m;
+	cin>>n>>m;
+	int l=n-1;
+	while(a--)
+	cout<<1;
+	cout<<endl;
+	while(b--)
+	cout<<8;
+	cout<<9<<endl;
+}
