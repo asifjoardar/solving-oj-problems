882a64e2f4b32218cc6046788cba16a2b72fd2c8
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-09-07 12:27:05 +0600

    Update B. Bash's Big Day.cpp

--- codeforce/B/B. Bash's Big Day.cpp
+++ codeforce/B/B. Bash's Big Day.cpp
@@ -1,39 +1,31 @@
-#include<iostream>
+#include<bits/stdc++.h>
+#define ll long long int
 using namespace std;
 main()
 {
-    int a[100001],i,j,n,count=0;
-    for(i=0;i<100001;i++) a[i]=1;
-    a[0]=0;a[1]=0;
-    for(i=4;i<100001;i+=2) a[i]=0;
-    for(i=3;i<100001;i++)
-    {
-        if(a[i]!=0)
-        {
-            for(j=3;j*i<100001;j++)
-                a[i*j]=0;
-        }
-    }
-    cin>>n;
-    int b[n],max=-1;
-    for(i=0;i<n;i++){
-        cin>>b[i];
-        if(b[i]>max) max=b[i];
-    }
-    for(i=2;i<10;i++)
-    {
-        if(i>max) break;
-        if(a[i]!=0)
-        {
-            for(j=0;j<n;j++)
-            {
-                if(i==b[j])
-                {
-                    count++;
-                    break;
-                }
-            }
-        }
-    }
-    cout<<(n-count)+1<<endl;
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	int n,i,x,ans=1,j;
+	map<int,int>a;
+	map<int,int>::iterator it;
+	cin>>n;
+	for(i=0;i<n;i++)
+	{
+		cin>>x;
+		for(j=2;j<=sqrt(x);j++)
+		{
+			if(x%j==0)
+				a[j]++;
+			while(x%j==0)
+				x/=j;
+		}
+		if(x>1) a[x]++;
+	}
+	for(it=a.begin();it!=a.end();it++)
+		{
+			ans=max(ans,it->second);
+		}
+	cout<<ans<<endl;
 }
