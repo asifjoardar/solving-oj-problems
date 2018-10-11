3fe91853832e2620b75749d6975ae5ff3d70dcbd
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-10-11 21:11:02 +0600

    Create B. Maximum Sum of Digits.cpp

---
+++ codeforce/B/B. Maximum Sum of Digits.cpp
@@ -0,0 +1,44 @@
+        /** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+ll sum(ll n)
+{
+    ll sum=0;
+    while(n>0)
+    {
+        sum+=n%10;
+        n/=10;
+    }
+    return sum;
+}
+main()
+{
+    ll n,p,q,num=9;
+    cin>>n;
+    if(n<=10)
+    {
+        cout<<n<<endl;
+        return 0;
+    }
+    while(num<n)
+    {
+        num*=10;
+        num+=9;
+    }
+    ll ans=(num-9)/10;
+    cout<<sum(ans)+sum(n-ans)<<endl;
+}
