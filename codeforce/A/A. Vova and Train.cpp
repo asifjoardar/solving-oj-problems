0288e73908ce6b38ad6504f88ec38f3fae874b9b
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-10-14 00:41:27 +0600

    Create A. Vova and Train.cpp

---
+++ codeforce/A/A. Vova and Train.cpp
@@ -0,0 +1,32 @@
+        /** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+main()
+{
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+    ll t,a,b,c,d,x,y,z;
+    cin>>t;
+    while(t--)
+    {
+    	cin>>a>>b>>c>>d;
+    	x=a/b;y=(c-1)/b;z=d/b;
+    	cout<<x-(z-y)<<endl;
+    }
+}
