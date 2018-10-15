a23fa623c117fe49923ec2e1bbde72d6305a4a6d
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-10-15 09:39:34 +0600

    Create C. Oh Those Palindromes.cpp

---
+++ codeforce/C/C. Oh Those Palindromes.cpp
@@ -0,0 +1,29 @@
+        /** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+main()
+{
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	ll t,a;
+	string s;
+	cin>>t>>s;
+	sort(s.begin(),s.end());
+	cout<<s<<endl;
+}
