f924c8a30489b54e790d785b424ac92339547fcc
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-10-19 04:00:07 +0600

    Create B. Appending Mex.cpp

---
+++ codeforce/B/B. Appending Mex.cpp
@@ -0,0 +1,36 @@
+/** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+main()
+{
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+    int n,m=-1,x,i;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+    	cin>>x;
+    	if(x>m+1)
+    	{
+    		cout<<i+1<<endl;
+    		return 0;    	}
+    	m=max(m,x);
+    }
+    cout<<-1<<endl;
+}
