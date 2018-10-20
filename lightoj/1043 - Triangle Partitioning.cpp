bebe2fe6a386d239de4ce9803c0c9f605a19f1da
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-10-20 08:10:21 +0600

    Create 1043 - Triangle Partitioning.cpp

---
+++ lightoj/1043 - Triangle Partitioning.cpp
@@ -0,0 +1,33 @@
+         /** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+main()
+{
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+    int i,t;
+    double ans,a,b,c,d;
+    cin>>t;
+    for(i=1;i<=t;i++)
+    {
+    	cin>>a>>b>>c>>d;
+    	ans=sqrt(d)/sqrt(d+1);
+    	printf("Case %d: %lf\n",i,ans*a);
+    }
+}
