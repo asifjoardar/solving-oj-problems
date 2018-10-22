aafcf9ac81e03a0e7e846b97f28289cc9fe5f5fd
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-10-23 01:38:11 +0600

    Create 10110 - Light, more light.cpp

---
+++ uva/10110 - Light, more light.cpp
@@ -0,0 +1,36 @@
+         /** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ui unsigned int
+using namespace std;
+main()
+{
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ui n;
+    while(cin>>n)
+    {
+    	if(n==0) break;
+    	ui p=sqrt(n);
+    	if(p*p==n)
+    		cout<<"yes\n";
+    	else cout<<"no\n";
+    }
+}
