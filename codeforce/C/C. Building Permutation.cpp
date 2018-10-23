919c1438c98295685fee5a83e5e9125d00c0bc0c
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-10-23 20:44:54 +0600

    Create C. Building Permutation.cpp

---
+++ codeforce/C/C. Building Permutation.cpp
@@ -0,0 +1,44 @@
+         /** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ui unsigned int
+using namespace std;
+main()
+{
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n,i,sum=0;
+    cin>>n;
+    ll a[n];
+    for(i=0;i<n;i++) cin>>a[i];
+    sort(a,a+n);
+	for(i=1;i<=n;i++)
+	{
+		if(a[i-1]<0)
+		{
+			sum+=abs(i-a[i-1]);
+		}
+		else
+		{
+			sum+=abs(a[i-1]-i);
+		}
+	}
+	cout<<sum<<endl;
+}
