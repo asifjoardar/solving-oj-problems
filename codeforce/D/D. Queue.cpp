90b1649b8c98f52f1be788f269fa5ff1a46e02a8
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-10-24 19:56:34 +0600

    Create D. Queue.cpp

---
+++ codeforce/D/D. Queue.cpp
@@ -0,0 +1,41 @@
+         /** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ui unsigned int
+using namespace std;
+main()
+{
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n,i,count=1,ans;
+    cin>>n;
+    ll a[n];
+    for(i=0;i<n;i++) cin>>a[i];
+    sort(a,a+n);
+	ans=a[0];
+    for(i=1;i<n;i++)
+    {
+    	if(ans<=a[i]){
+    		count++;
+    		ans+=a[i];
+    	}
+    }
+    cout<<count<<endl;
+}
