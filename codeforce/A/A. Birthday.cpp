5349b81c792eca7cee55c857949ac5661090b174
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-10-26 15:54:06 +0600

    Create A. Birthday.cpp

---
+++ codeforce/A/A. Birthday.cpp
@@ -0,0 +1,37 @@
+         /** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ui unsigned int
+using namespace std;
+main()
+{
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n,m,k,l,ans,p;
+    cin>>n>>m>>k>>l;
+    p=k+l;
+    ans=(p/m);
+    if(p%m!=0)
+    	ans++;
+    if(n<ans*m||n-k<l)
+    	cout<<-1<<endl;
+    else
+    	cout<<ans<<endl;
+}
