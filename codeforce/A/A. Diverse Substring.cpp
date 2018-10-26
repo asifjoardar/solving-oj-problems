2cdc7201e3fb49723b6c7a1b2f4c91886b7f3190
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-10-26 15:55:30 +0600

    Create A. Diverse Substring.cpp

---
+++ codeforce/A/A. Diverse Substring.cpp
@@ -0,0 +1,39 @@
+         /** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ui unsigned int
+using namespace std;
+main()
+{
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,i;
+    string s;
+    cin>>n>>s;
+    for(i=0;i<n-1;i++)
+    {
+    	if(s[i]!=s[i+1])
+    	{
+    		cout<<"YES\n"<<s[i]<<s[i+1]<<endl;
+    		return 0;
+    	}
+    }
+    cout<<"NO\n";
+}
