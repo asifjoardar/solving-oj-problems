632b7782804406e9244d1a0772b543c05288b102
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-10-26 15:57:44 +0600

    Create B. LCM.cpp

---
+++ codeforce/B/B. LCM.cpp
@@ -0,0 +1,40 @@
+         /** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ui unsigned int
+using namespace std;
+main()
+{
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n,i,count=0;
+    cin>>n;
+    for(i=1;i<=sqrt(n);i++)
+    {
+    	if(n%i==0)
+    	{
+    		if(n/i==i)
+    			count++;
+    		else
+    			count+=2;
+    	}
+    }
+    cout<<count<<endl;
+}
