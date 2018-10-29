5cefd692e8bbbbbc103d7f73951ccb7cd1328c1c
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-10-29 09:50:12 +0600

    Create A. Elections.cpp

---
+++ codeforce/A/A. Elections.cpp
@@ -0,0 +1,44 @@
+         /** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ui unsigned int
+using namespace std;
+main()
+{
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int m=-1,n,i,sum=0;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++){ 
+    	cin>>a[i];
+    	sum+=a[i];
+    	m=max(a[i],m);
+    }
+    while(1)
+    {
+    	if(((m*n)-sum)>sum)
+    	{
+    		cout<<m<<endl;
+    		return 0;
+    	}
+    	m++;
+    }
+}
