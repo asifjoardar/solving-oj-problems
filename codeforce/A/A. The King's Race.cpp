04c9cc416559dfae501127593ab368fcc0e8d1f4
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-11-06 18:17:12 +0600

    Create A. The King's Race.cpp

---
+++ codeforce/A/A. The King's Race.cpp
@@ -0,0 +1,35 @@
+/** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ui unsigned int
+using namespace std;
+main()
+{
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n,a,b,p,q;
+    cin>>n>>a>>b;
+    p=abs(1-a)+abs(1-b);
+    q=abs(n-a)+abs(n-b);
+    if(p<=q)
+    	cout<<"White\n";
+    else
+    	cout<<"Black\n";
+}
