ecd556493e56aae444d7c2f9a53608e8a61c6238
asifjoardar <35629053+asifjoardar@users.noreply.github.com>
2018-11-29 16:09:47 +0600

    Create 1877. Bicycle Codes.cpp

---
+++ timus/1877. Bicycle Codes.cpp
@@ -0,0 +1,32 @@
+/** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+
+main()
+{
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	string a,b;
+	cin>>a>>b;
+	if((a[3]-'0')%2==0||(b[3]-'0')%2!=0)
+		cout<<"yes\n";
+	else
+		cout<<"no\n";
+}
