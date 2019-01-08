11c6f661c0b3c6f04e9c4ea7e78d186c27c210ef
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2019-01-08 22:35:34 +0600

    Create  1245 - Harmonic Number (II) .cpp

---
+++ lightoj/1245 - Harmonic Number (II) .cpp
@@ -0,0 +1,40 @@
+        /** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll unsigned long long int
+using namespace std;
+main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	ll t,i,n,j;
+	cin>>t;
+	for(j=1;j<=t;j++)
+	{
+		cin>>n;
+		ll ans=n,p=n,q;
+		for(i=2;i<=p;i++)
+		{
+			q=n/i;
+			ans+=(p-q)*(i-1);
+			if(n/i>i-1)
+				ans+=n/i;
+			p=q;
+		}
+		printf("Case %lld: %lld\n",j,ans);
+	}
+}
