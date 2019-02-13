2bf05ab3b9c251bac74540584baba054cf5b7d6c
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2019-02-13 15:45:42 +0600

    Rename 11991 - Easy Problem from Rujia Liu.cpp to 11991 - Easy Problem from Rujia Liu .cpp

---
+++ uva/11991 - Easy Problem from Rujia Liu .cpp
@@ -0,0 +1,57 @@
+/** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI 3.141592653589793
+#define white 1
+#define gray 2
+#define black 3
+#define PII pair<ll, ll> 
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    fio();
+    int n,m,i,x,y;
+    while(cin>>n>>m)
+    {
+    	vector<int>v[1000005];
+    	for(i=0;i<n;i++)
+    	{
+    		cin>>x;
+    		v[x].pb(i+1);
+    	}
+    	for(i=0;i<m;i++)
+    	{
+    		cin>>x>>y;
+    		if(v[y].size()>=x)
+    			cout<<v[y][x-1]<<endl;
+    		else
+    			cout<<0<<endl;
+    	}
+    }
+}
