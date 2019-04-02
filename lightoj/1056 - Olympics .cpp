6584ba2cc5f81c71b9f42b70a3624cefcea68212
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2019-04-03 00:07:04 +0600

    Create 1056 - Olympics .cpp

---
+++ lightoj/1056 - Olympics .cpp
@@ -0,0 +1,52 @@
+/** BiSmIlLaHiR rAhMaNiR rAhIm **\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI 3.141592653589793
+#define white 1
+#define gray 2
+#define black 3
+#define PII pair<ll, ll> 
+#define SIZE 1000005
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+
+main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    fio();
+    ll t,i;
+    cin>>t;
+    for(i=1;i<=t;i++)
+    {
+        double a,b,r,theta,s,x;
+        char c;
+        cin>>a>>c>>b;
+        r=sqrt((a*a)+(b*b))/2.0;
+        theta=acos(((r*r)+(r*r)-(b*b))/(2*r*r));
+        s=(theta*2*180*r)/360.0;
+        x=400.0/((2*s)+(2*a));
+        pf("Case %lld: %.10lf %.10lf\n",i,a*x,b*x);
+    }
+}
