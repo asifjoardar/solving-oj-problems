1273785692009c673cbb40622bb2847d25c40f15
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2019-09-21 10:46:59 +0600

    Create Dr. Tanzil's Island.cpp

---
+++ toph/Dr. Tanzil's Island.cpp
@@ -0,0 +1,48 @@
+/** BiSmIlLaHiR rAhMaNiR rAhIm **\
+ 
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+ 
+            \**  DIIT(17th batch) **/
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define endl "\n"
+#define pb push_back
+#define eb emplace_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI 2*acos(0.0)
+#define PII pair<ll, ll>
+#define pii pair<int, int>
+#define what_is(x) cerr<<#x<<" is "<<x<<endl;
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+int main()
+{
+    /*#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    fio();*/
+    ll q,i,j,n;
+    cin>>q;
+    while(q--)
+    {
+        double x;
+        cin>>x;
+        printf("%.6lf\n",PI*x*x);
+    }
+    return 0;
+}
