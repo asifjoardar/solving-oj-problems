4761f14c147b1455136bc73d3efb2741bebb0c8a
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2019-10-04 14:41:56 +0600

    Create I.cpp

---
+++ codemarshal/icpc dhaka regional/2019/I.cpp
@@ -0,0 +1,42 @@
+/** BiSmIlLaHiR rAhMaNiR rAhIm **\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI 3.141592653589793
+#define white 1
+#define gray 2
+#define black 3
+#define PII pair<ll, ll>
+#define SIZE 1000005
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ll fx[]={1,0,-1,0,-1,-1,1,1};
+ll fy[]={0,1,0,-1,-1,1,-1,1};
+
+
+int main()
+{
+    /*#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif*/
+    cout<<"Welcome to ICPC Dhaka Regional Online Preliminary Contest, 2019\n";
+    return 0;
+}
