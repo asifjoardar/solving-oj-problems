84b2d8380ac8b61508c98cad53a224d3179a20da
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2020-05-21 02:23:44 +0600

    Create Permutations.cpp

---
+++ CSES/introductory problems/Permutations.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define mkp make_pair
+#define pii pair<int,int>
+#define ss second
+#define fs first
+#define endl << "\n"
+using namespace std;
+
+void sol(ll n){
+	ll i=5;
+	if(n%2==0) i=n-1;
+	for(;i>=1;i-=2)
+	{
+		cout<<i<<" ";
+	}
+	cout<<"4 2 ";
+	for(i=6;i<=n;i++)
+	    cout<<i<< " ";
+	cout endl;
+}
+
+int main() {
+	ll n;
+	cin>>n;
+	if(n==1) cout<<1 endl,exit(0);
+	else if(n<4) cout<<"NO SOLUTION\n",exit(0);
+	sol(n);
+	return 0;
+}
