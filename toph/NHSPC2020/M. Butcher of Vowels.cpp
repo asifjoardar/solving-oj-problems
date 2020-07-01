12a29c6d1ec132cbf802e9073e1f02db07dcc39d
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2020-07-02 03:47:44 +0600

    Create M. Butcher of Vowels.cpp

---
+++ toph/NHSPC2020/M. Butcher of Vowels.cpp
@@ -0,0 +1,55 @@
+/********** BiSmIlLaHiR rAhMaNiR rAhIm ***********\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+\*************** DIIT(17th batch) ****************/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI acos(-1.0)
+#define PII pair<ll , ll>
+#define SIZE 1000005
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ll fx[]={1,0,-1,0,-1,-1,1,1};
+ll fy[]={0,1,0,-1,-1,1,-1,1};
+
+int main()
+{
+   
+    //fio();
+    ll t;
+    cin>>t;
+    while(t--){
+    	ll i,j;
+    	string str="aeiou",s;
+    	bool ok=false;
+    	cin>>s;
+    	for(i=0;i<str.size();i++)
+    	{
+    		for(j=0;j<s.size();j++)
+    		{
+    			if(s[j]==str[i]){
+    				ok=true;
+    				break;
+    			}
+    		}
+    	}
+    	if(ok) cout<<"Yes\n";
+    	else cout<<"No\n";
+    }
+    return 0;
+}
