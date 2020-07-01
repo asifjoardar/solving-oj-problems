f4864e542c630771e2df7e83715cfb061aa3833d
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2020-07-02 03:45:53 +0600

    Create I. Code Generator.cpp

---
+++ toph/NHSPC2020/I. Code Generator.cpp
@@ -0,0 +1,48 @@
+/********** BiSmIlLaHiR rAhMaNiR rAhIm ***********\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+\*************** DIIT(17th batch) ****************/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI acos(-1.0)
+#define PII pair<ll , ll>
+#define SIZE 1000005
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ll fx[]={1,0,-1,0,-1,-1,1,1};
+ll fy[]={0,1,0,-1,-1,1,-1,1};
+
+int main()
+{
+    
+    //fio();
+    ll n,i,j,k,l,count=0;
+    string s;
+    ll c=0,o=0,d=0,e=0;
+    cin>>n>>s;
+    for(i=0;i<n;i++){
+        if(s[i]=='c') c++;
+        else if(s[i]=='o' and o+1<=c) o++;
+        else if(s[i]=='d' and d+1<=o) d++;
+        else if(s[i]=='e' and e+1<=d){
+            e++;
+        }
+    }
+    cout<<e<<endl;
+    return 0;
+}
