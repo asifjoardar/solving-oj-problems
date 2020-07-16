f652b22bc0df5ab43312670eda0efa51d47dbff2
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2020-07-16 22:24:36 +0600

    Update Maximum Subarray Sum(n).cpp

--- CSES/Sorting and Searching/Maximum Subarray Sum(n).cpp
+++ CSES/Sorting and Searching/Maximum Subarray Sum(n).cpp
@@ -1,1 +1,52 @@
+/********** BiSmIlLaHiR rAhMaNiR rAhIm ***********\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+\*************** DIIT(17th batch) ****************/
 
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define fs first
+#define ss second
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI acos(-1.0)
+#define PII pair<ll , ll>
+#define SIZE 200000000
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ll fx[]={1,0,-1,0,-1,-1,1,1};
+ll fy[]={0,1,0,-1,-1,1,-1,1};
+
+int main()
+{
+    fio();
+    /*#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif*/
+    ll n,i;
+    cin>>n;
+    ll a[n+1];
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    ll ans=a[0],sum=a[0];
+    for(i=1;i<n;i++){
+        sum=max(a[i]+sum,a[i]);
+        ans=max(ans,sum);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
