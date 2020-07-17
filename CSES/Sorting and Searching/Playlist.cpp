ef3363a7df1ee731c26409cb2884d62bdf0b8483
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2020-07-17 19:13:36 +0600

    Update Playlist.cpp

--- CSES/Sorting and Searching/Playlist.cpp
+++ CSES/Sorting and Searching/Playlist.cpp
@@ -1,1 +1,57 @@
+/********** BiSmIlLaHiR rAhMaNiR rAhIm ***********\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+\*************** DIIT(17th batch) ****************/
 
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define fs first
+#define ss second
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI acos(-1.0)
+#define PII pair<ll , ll>
+#define SIZE 200000000
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ll fx[]={1,0,-1,0,-1,-1,1,1};
+ll fy[]={0,1,0,-1,-1,1,-1,1};
+
+int main()
+{
+    fio();
+    /*#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif*/
+    ll n,i,num,ans=1,count=0;
+    cin>>n;
+    ll a[n];
+    map<ll,ll>mp;
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    mp[a[0]]=1;
+    ll last=1;
+    for(i=1;i<n;i++){
+        if(mp[a[i]]!=0 and last<=mp[a[i]]){
+            last=mp[a[i]]+1;
+        }
+        mp[a[i]]=i+1;
+        ans=max(ans,abs(last-(i+1))+1);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
