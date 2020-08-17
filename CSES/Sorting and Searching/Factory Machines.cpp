6f21d9a8633de1244f946e09d692278345b7d17c
MD. Asif Joardar <mrsparrow04@gmail.com>
2020-08-17 17:18:29 +0600

    Create Factory Machines.cpp

---
+++ CSES/Sorting and Searching/Factory Machines.cpp
@@ -0,0 +1,62 @@
+/********** BiSmIlLaHiR rAhMaNiR rAhIm ***********\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+\*************** DIIT(17th batch) ****************/
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define fs first
+#define ss second
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI acos(-1.0)
+#define PII pair<ll , ll>
+#define ki_ase(x,y) cout<<x<<" "<<y<<endl;
+#define SIZE 200000000
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ll fx[]={1,0,-1,0,-1,-1,1,1};
+ll fy[]={0,1,0,-1,-1,1,-1,1};
+ 
+ 
+int main()
+{
+    //fio();
+    /*#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif*/
+    ll n,i,t;
+    cin>>n>>t;
+    ll a[n];
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    ll l=1,r=1e18;
+    while(l<=r){
+        ll mid=l+(r-l)/2;
+        ll cnt=0;
+        for(i=0;i<n;i++){
+            cnt+=(mid/a[i]);
+            if(cnt>=t) break;
+        }
+        if(cnt>=t)
+            r=mid-1;
+        else
+            l=mid+1;
+    }
+    cout<<r+1<<endl;
+    return 0;
+}
