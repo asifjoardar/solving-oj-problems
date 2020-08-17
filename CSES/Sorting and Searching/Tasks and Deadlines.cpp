10a20601b12726c52c2c5f2e31e1af19db9e5795
MD. Asif Joardar <mrsparrow04@gmail.com>
2020-08-17 19:30:43 +0600

    Create Tasks and Deadlines.cpp

---
+++ CSES/Sorting and Searching/Tasks and Deadlines.cpp
@@ -0,0 +1,56 @@
+/********** BiSmIlLaHiR rAhMaNiR rAhIm ***********\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+\*************** DIIT(17th batch) ****************/
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define fs first
+#define ss second
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI acos(-1.0)
+#define PII pair<ll , ll>
+#define ki_ase(x,y) cout<<x<<" "<<y<<endl;
+#define SIZE 200000000
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ll fx[]={1,0,-1,0,-1,-1,1,1};
+ll fy[]={0,1,0,-1,-1,1,-1,1};
+ 
+ 
+int main()
+{
+    //fio();
+    /*#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif*/
+    ll n,i,x,y,sum=0,ans=0;
+    cin>>n;
+    vector<PII>v;
+    for(i=0;i<n;i++){
+        cin>>x>>y;
+        v.pb({x,y});
+    }
+    sort(v.begin(),v.end());
+    for(i=0;i<n;i++){
+        sum+=v[i].fs;
+        ans+=v[i].ss-sum;
+        //cout<<ans<<endl;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
