6ad7fef89db379be37d85fe8d474a5e3e8c3f0c3
MD. Asif Joardar <mrsparrow04@gmail.com>
2020-08-17 23:17:50 +0600

    Create Reading Books.cpp

---
+++ CSES/Sorting and Searching/Reading Books.cpp
@@ -0,0 +1,54 @@
+/********** BiSmIlLaHiR rAhMaNiR rAhIm ***********\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+\*************** DIIT(17th batch) ****************/
+ 
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define fs first
+#define ss second
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI acos(-1.0)
+#define PII pair<ll , ll>
+#define ki_ase(x,y) cout<<x<<" "<<y<<endl;
+#define SIZE 200000000
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ll fx[]={1,0,-1,0,-1,-1,1,1};
+ll fy[]={0,1,0,-1,-1,1,-1,1};
+ 
+ 
+int main()
+{
+    //fio();
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    ll n,i,mx=0,sum=0;
+    cin>>n;
+    vector<ll>a(n),b(n);
+    for(i=0;i<n;i++){
+        cin>>a[i];
+        mx=max(a[i],mx);
+        sum+=a[i];
+    }
+    if((sum-mx)<=mx)
+        cout<<mx*2<<endl;
+    else
+        cout<<sum<<endl;
+    return 0;
+}
