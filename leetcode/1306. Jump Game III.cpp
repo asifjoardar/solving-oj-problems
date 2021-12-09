3c5963d74f9f9d2b9d84cfd7ff3ed6d5350e5557
Md. Asif Joardar <mrsparrow04@gmail.com>
2021-12-09 21:08:46 +0600

    Create 1306. Jump Game III.cpp

---
+++ leetcode/1306. Jump Game III.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool dfs(vector<int>& arr, vector<bool>& vis, int i) {
+        if (i < 0 || i >= arr.size() || vis[i]) return false;
+        if (arr[i] == 0) return true;
+        vis[i] = true;
+        return dfs(arr, vis, i + arr[i]) || dfs(arr, vis, i - arr[i]);
+    }
+    
+    bool canReach(vector<int>& arr, int start) {
+        vector<bool> vis(arr.size(), false);
+        return dfs(arr, vis, start);
+    }
+};
